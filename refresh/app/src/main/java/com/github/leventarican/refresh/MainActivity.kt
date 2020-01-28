bce4f164ee6e35c338af3e63e60743d66815f7b3
leventarican <levent@protonmail.com>
2020-01-28 23:20:27 +0100

    layout, button, event, ...

--- refresh/app/src/main/java/com/github/leventarican/refresh/MainActivity.kt
+++ refresh/app/src/main/java/com/github/leventarican/refresh/MainActivity.kt
@@ -2,11 +2,28 @@ package com.github.leventarican.refresh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val resultText: TextView = findViewById(R.id.tv_result)
+        val rollButton: Button = findViewById(R.id.bt_roll)
+
+        rollButton.setOnClickListener {
+            roll()
+            resultText.text = "rolled: ${(1..6).random()}"
+        }
+
+
+    }
+
+    private fun roll(): Unit {
+        Toast.makeText(this, "button roll clicked", Toast.LENGTH_LONG).show()
     }
 }
