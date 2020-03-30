cd89825677efbb78c6d69cfe9a9152807b70b2fa
leventarican <levent@protonmail.com>
2020-03-30 23:47:01 +0200

    bouncing

--- fundamentals/refresh/app/src/main/java/com/github/leventarican/refresh/Page6.kt
+++ fundamentals/refresh/app/src/main/java/com/github/leventarican/refresh/Page6.kt
@@ -4,6 +4,7 @@ import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.BounceInterpolator
 import android.view.animation.LinearInterpolator
 import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_page6.*
@@ -20,11 +21,12 @@ class Page6 : AppCompatActivity() {
         val factorDPtoPX = resources.displayMetrics.density
         val animate = findViewById<ImageView>(R.id.iv_animate)
 
-        val va = ValueAnimator.ofFloat(0.0f, 340 * factorDPtoPX)
-        va.interpolator = LinearInterpolator()
-        va.duration = 3000L
+        val va = ValueAnimator.ofFloat(0.0f, 100 * factorDPtoPX)
+//        va.interpolator = LinearInterpolator()
+        va.interpolator = BounceInterpolator()
+        va.duration = 2000L
         va.repeatMode = ObjectAnimator.REVERSE
-        va.repeatCount = 1
+        va.repeatCount = 3
         va.addUpdateListener { animation ->
             iv_animate.translationY = animation.animatedValue as Float
         }
